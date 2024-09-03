c0bb547a9508775d0097f889d9a0db1cc92ecf73
CamrynBowWow <bowdencamryn@gmail.com>
2024-09-03 09:24:08 +0200

    Application Shell Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import ThemeToggle from '@/components/theme-toggle'
 import React from 'react'
 
 export default function Home() {
   return (
     <section className='py-24'>
-      <div className='container'>
+      <div className='container max-w-3xl'>
         <h1 className='text-5xl font-bold'>Portfolio</h1>
-        <ThemeToggle />
       </div>
     </section>
   )
